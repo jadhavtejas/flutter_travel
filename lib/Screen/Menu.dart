242316113efd579171c61d85756f73a64eff6846
Jadhav Tejas <61315447+jadhavtejas@users.noreply.github.com>
2022-08-23 15:14:07 +0530

    Tejas

---
+++ lib/Screen/Menu.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Menu extends StatefulWidget {
+  Menu({Key? key}) : super(key: key);
+
+  @override
+  State<Menu> createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: CircleAvatar(child: Image.asset('images.png')),);
+  }
+}
