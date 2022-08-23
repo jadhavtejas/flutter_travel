b5985c9ca237a4f8a7883856535c22052a0e0021
Jadhav Tejas <61315447+jadhavtejas@users.noreply.github.com>
2022-08-23 20:28:23 +0530

    Tejas1

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,6 @@ import 'dart:js';
 
 import 'package:flutter/material.dart';
 import 'package:travel_ui_stf/Screen/BottomNavigation.dart';
-import 'package:travel_ui_stf/Screen/Home.dart';
-import 'package:travel_ui_stf/Screen/Information.dart';
-import 'package:travel_ui_stf/Screen/Search.dart';
-import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,24 +15,4 @@ class MyApp extends StatelessWidget {
         home: MyStatefulWidget(),
         debugShowCheckedModeBanner: false,
       );
-}
-
-class ViewCh extends StatefulWidget {
-  ViewCh({Key? key}) : super(key: key);
-
-  @override
-  State<ViewCh> createState() => _ViewChState();
-}
-
-class _ViewChState extends State<ViewCh> {
-  bool isDesktop(BuildContext context) =>
-      MediaQuery.of(context).size.width >= 600;
-
-  bool isMobile(BuildContext context) =>
-      MediaQuery.of(context).size.width < 600;
-      
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
 }
