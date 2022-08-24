8d93fc91148c572e1f0686ccd0c08f1c3e9b2a91
Jadhav Tejas <61315447+jadhavtejas@users.noreply.github.com>
2022-08-24 18:04:37 +0530

    Tejasupdate_icon

--- lib/Common/AppImages.dart
+++ lib/Common/AppImages.dart
@@ -1,8 +1,10 @@
 class AppImages {
+  // ignore: constant_identifier_names
   static const String CircleAvtarHome = "assets/images.png";
-  static const String HomeTopIcon = "assets/HomeTopIcon.png";
+  // ignore: constant_identifier_names
+  // static const String HomeTopIcon = "assets/HomeTopIcon.png";
   // static const String  = "assets/images.jpg";
-//   static const String = "assets/Recomended.jpg";
+  static const String HomeTopIcon = "HomeIconTop.png";
 //   static const String = "assets/Recomended1.jpg";
 //   static const String = "assets/search1.jpg";
 //   static const String = "assets/search2.jpg";
