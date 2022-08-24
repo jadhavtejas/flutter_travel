18da8405cbfd5e7a4d0fd9bc9a9d80853f3b2729
Varshavjadhav <91647253+Varshavjadhav@users.noreply.github.com>
2022-08-24 17:27:00 +0530

    varsha6

--- lib/main.dart
+++ lib/main.dart
@@ -14,29 +14,5 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) => MaterialApp(
         home: MyStatefulWidget(),
         debugShowCheckedModeBanner: false,
-        theme: ThemeData(fontFamily: 'Avenir'),
       );
 }
-<<<<<<< HEAD
-
-class ViewCh extends StatefulWidget {
-  ViewCh({Key? key}) : super(key: key);
-
-  @override
-  State<ViewCh> createState() => _ViewChState();
-}
-
-class _ViewChState extends State<ViewCh> {
-  bool isDesktop(BuildContext context) =>
-      MediaQuery.of(context).size.width >= 600;
-
-  bool isMobile(BuildContext context) =>
-      MediaQuery.of(context).size.width < 600;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-=======
->>>>>>> 095e22f3456e5f7a2c3cdd9794b72327034fbfac
